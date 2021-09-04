ac80117e364482d1f5569e5e9bf7165861ade6f5
Zhang Ran <info.zhangran@gmail.com>
2021-09-04 15:46:26 +0800

    Add full integration apps with https traffic

--- full-integation/microservices-backend-springboot/src/main/java/com/example/keycloak/springbootkeycloak/controller/ZenikaController.java
+++ full-integation/microservices-backend-springboot/src/main/java/com/example/keycloak/springbootkeycloak/controller/ZenikaController.java
@@ -3,7 +3,9 @@ package com.example.keycloak.springbootkeycloak.controller;
 import javax.annotation.security.RolesAllowed;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,11 @@ public class ZenikaController {
       return new ResponseEntity<>(
           "Valid user", HttpStatus.OK);
     }
+
+  @RequestMapping("/hello")
+  public ResponseEntity<String> insecure() {
+    return new ResponseEntity<>(
+        "insecure call", HttpStatus.OK);
+  }
 
 }
